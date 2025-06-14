ad2b3bc3ce504753b812a4ec05b79afd26dd780c
Priyanshu Kashyap <priyanshu24k@gmail.com>
2025-06-14 22:26:14 +0530

    Time: 0 ms (100%), Space: 9.6 MB (91.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        int count[26] = {0}; // for lowercase English letters
+
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        for (char c : t) {
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0) return false;
+        }
+
+        return true;
+    }
+};
